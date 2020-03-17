57667681d69e5848c80698615e52c9fb5c6276c2
Amritansh <amritansh.p@reculta.com>
2020-03-18 00:59:24 +0530

    Context API added

--- expense-tracker/src/components/Transaction.js
+++ expense-tracker/src/components/Transaction.js
@@ -1,27 +1,11 @@
-import React, {useState} from 'react'
+import React from 'react'
 
-const Transaction = () => {
-const [text, setText] = useState('');
-const [amount, setamount] = useState(0);
-
+const Transaction = ({ transaction }) => {
+    const sign = transaction.amount < 0 ? '-':'+';
     return (
-        <div>
-            <h3>Add new transaction</h3>
-      <form id="form">
-        <div className="form-control">
-          <label htmlFor="text">Text</label>
-          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
-        </div>
-        <div className="form-control">
-          <label htmlFor="amount"
-            >Amount <br />
-            (negative - expense, positive - income)</label
-          >
-          <input type="number"  value={amount} onChange={(e) => setamount(e.target.value)} placeholder="Enter amount..." />
-        </div>
-        <button className="btn">Add transaction</button>
-      </form>
-        </div>
+        <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+                  {transaction.text} <span>{sign} $ {Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
+             </li>
     )
 }
 
