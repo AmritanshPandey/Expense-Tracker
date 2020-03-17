a55344b24354eafc38bdb6d98c04236171e6bcda
Amritansh <amritansh.p@reculta.com>
2020-03-17 23:03:57 +0530

    basic ui

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import Transaction from './components/Transaction';
 
 function App() {
   return (
     <div>
-      Hello!
+      <Header />
+      <div className="container">
+            <Balance />
+            <IncomeExpenses />
+            <TransactionList />
+            <Transaction />
+        </div>
     </div>
+
   );
 }
 
